609b5c0405f7d93df02c215a920026c5a69c683e
Gerardo8 <gerardo.lopezre@gmail.com>
2016-09-22 16:19:08 -0500

    Initial commit

---
+++ src/main/java/com/bidg/exam/config/ApplicationConfiguration.java
@@ -0,0 +1,19 @@
+package com.bidg.exam.config;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by gerardo8 on 22/09/16.
+ */
+@Configuration
+public class ApplicationConfiguration {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.setPort(9000);
+        return tomcat;
+    }
+}
